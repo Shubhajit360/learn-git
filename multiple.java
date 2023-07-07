68a4ee058d0519d245d655cfa755b2ec019680fc
Shubhajit Debnath <shubhajit22debnath2002@gmail.com>
2023-07-07 18:50:40 +0530

    add

---
+++ multiple.java
@@ -0,0 +1,11 @@
+public public class multiple{
+    public static void main(String[] args) {
+      int a=5;
+      int b=12;
+      int sum;
+      sum=a+b;
+       System.out.println("sum="+sum);
+         
+     }
+    
+}
